56341a15afd790daa5a372b590fb358edbcfea0a
Vavassor <copernicus-@hotmail.com>
2017-02-07 18:44:54 -0500

    Fixed: crash on splash screen on version Kitkat and under due to trying to load a vector drawable in a selector.

--- app/src/main/java/com/keylesspalace/tusky/SplashActivity.java
+++ app/src/main/java/com/keylesspalace/tusky/SplashActivity.java
@@ -20,6 +20,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 
 public class SplashActivity extends AppCompatActivity {
     @Override
