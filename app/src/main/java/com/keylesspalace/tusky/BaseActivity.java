ce04c7ab2b2fddd930e3f94ff8924e8c7d57b51c
Eugen Rochko <eugen@zeonfederated.com>
2017-03-07 13:05:51 +0100

    Sliding animation for activity transitions

--- app/src/main/java/com/keylesspalace/tusky/BaseActivity.java
+++ app/src/main/java/com/keylesspalace/tusky/BaseActivity.java
@@ -15,6 +15,7 @@
 
 package com.keylesspalace.tusky;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
@@ -36,6 +37,26 @@ public class BaseActivity extends AppCompatActivity {
         if (PreferenceManager.getDefaultSharedPreferences(this).getBoolean("lightTheme", false)) {
             setTheme(R.style.AppTheme_Light);
         }
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransitionExit();
+    }
+
+    @Override
+    public void startActivity(Intent intent) {
+        super.startActivity(intent);
+        overridePendingTransitionEnter();
+    }
+
+    private void overridePendingTransitionEnter() {
+        overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
+    }
+
+    private void overridePendingTransitionExit() {
+        overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
     }
 
     @Override
