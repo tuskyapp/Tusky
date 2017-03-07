34c4951241e23deb059143dcca52e0f4b825abb3
Eugen Rochko <eugen@zeonfederated.com>
2017-03-07 23:10:41 +0100

    Animated splash screen (mind: preview for the circular loader seems broken in Android Studio, but works in app)

--- app/src/main/java/com/keylesspalace/tusky/SplashActivity.java
+++ app/src/main/java/com/keylesspalace/tusky/SplashActivity.java
@@ -15,29 +15,43 @@
 
 package com.keylesspalace.tusky;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
+    private static int SPLASH_TIME_OUT = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
         /* Determine whether the user is currently logged in, and if so go ahead and load the
          * timeline. Otherwise, start the activity_login screen. */
         SharedPreferences preferences = getSharedPreferences(
                 getString(R.string.preferences_file_key), Context.MODE_PRIVATE);
         String domain = preferences.getString("domain", null);
         String accessToken = preferences.getString("accessToken", null);
-        Intent intent;
+
+
+        final Intent intent;
+
         if (domain != null && accessToken != null) {
             intent = new Intent(this, MainActivity.class);
         } else {
             intent = new Intent(this, LoginActivity.class);
         }
-        startActivity(intent);
-        finish();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
