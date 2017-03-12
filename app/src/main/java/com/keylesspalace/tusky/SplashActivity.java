4fd941febdfa4f2163e6acd291fd2d802db68cfa
Eugen Rochko <eugen@zeonfederated.com>
2017-03-12 14:07:21 +0100

    Full screen splash

--- app/src/main/java/com/keylesspalace/tusky/SplashActivity.java
+++ app/src/main/java/com/keylesspalace/tusky/SplashActivity.java
@@ -22,6 +22,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends Activity {
     private static int SPLASH_TIME_OUT = 2000;
@@ -29,7 +31,12 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash);
+        
         /* Determine whether the user is currently logged in, and if so go ahead and load the
          * timeline. Otherwise, start the activity_login screen. */
         SharedPreferences preferences = getSharedPreferences(
