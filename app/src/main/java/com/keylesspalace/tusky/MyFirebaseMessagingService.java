4d9fd09a3c39e73afd377ff9c0b1c53219af5e2e
Vavassor <copernicus-@hotmail.com>
2017-04-09 20:12:31 -0400

    License change back to GPL 3

--- app/src/main/java/com/keylesspalace/tusky/MyFirebaseMessagingService.java
+++ app/src/main/java/com/keylesspalace/tusky/MyFirebaseMessagingService.java
@@ -1,3 +1,23 @@
+/* Copyright 2017 Andrew Dawson
+ *
+ * This file is a part of Tusky.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Tusky is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Tusky; if not,
+ * see <http://www.gnu.org/licenses>.
+ *
+ * If you modify this Program, or any covered work, by linking or combining it with Firebase Cloud
+ * Messaging and Firebase Crash Reporting (or a modified version of those libraries), containing
+ * parts covered by the Google APIs Terms of Service, the licensors of this Program grant you
+ * additional permission to convey the resulting work. */
+
 package com.keylesspalace.tusky;
 
 import android.app.NotificationManager;
