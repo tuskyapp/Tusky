87b34df892d39a53abdda5606e5cbb37b8f7e6ba
Conny Duck <k.pozniak@gmx.at>
2017-10-23 21:50:41 +0200

    fixes a crash within TootDao that is caused by an change in the room library

--- app/src/main/java/com/keylesspalace/tusky/TuskyApplication.java
+++ app/src/main/java/com/keylesspalace/tusky/TuskyApplication.java
@@ -54,6 +54,7 @@ public class TuskyApplication extends Application {
 
         db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "tuskyDB")
                 .allowMainThreadQueries()
+                .addMigrations(AppDatabase.MIGRATION_2_3)
                 .build();
     }
 }
--- app/src/main/java/com/keylesspalace/tusky/db/AppDatabase.java
+++ app/src/main/java/com/keylesspalace/tusky/db/AppDatabase.java
@@ -1,14 +1,30 @@
 package com.keylesspalace.tusky.db;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.migration.Migration;
 
 /**
  * DB version & declare DAO
  */
 
-@Database(entities = {TootEntity.class}, version = 2, exportSchema = false)
+@Database(entities = {TootEntity.class}, version = 3, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
 
     public abstract TootDao tootDao();
+
+    public static final Migration MIGRATION_2_3 = new Migration(2, 3) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+            //this migration is necessary because of a change in the room library
+            database.execSQL("CREATE TABLE TootEntity2 (uid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, text TEXT, urls TEXT, contentWarning TEXT);");
+
+            database.execSQL("INSERT INTO TootEntity2 SELECT * FROM TootEntity;");
+            database.execSQL("DROP TABLE TootEntity;");
+            database.execSQL("ALTER TABLE TootEntity2 RENAME TO TootEntity;");
+
+        }
+    };
+
 }
