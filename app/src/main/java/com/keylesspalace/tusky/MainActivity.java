6ca6abe9592577fcecc191d99b979f59cc86a698
Conny Duck <k.pozniak@gmx.at>
2017-11-13 19:45:28 +0100

    simplify MainActivity back press handling

--- app/src/main/java/com/keylesspalace/tusky/MainActivity.java
+++ app/src/main/java/com/keylesspalace/tusky/MainActivity.java
@@ -22,7 +22,7 @@ import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
@@ -59,7 +59,6 @@ import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -83,7 +82,6 @@ public class MainActivity extends BaseActivity implements ActionButtonActivity {
     private FloatingActionButton composeButton;
     private String loggedInAccountId;
     private String loggedInAccountUsername;
-    private Stack<Integer> pageHistory;
     private AccountHeader headerResult;
     private Drawer drawer;
     private ViewPager viewPager;
@@ -92,14 +90,6 @@ public class MainActivity extends BaseActivity implements ActionButtonActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        pageHistory = new Stack<>();
-        if (savedInstanceState != null) {
-            List<Integer> restoredHistory = savedInstanceState.getIntegerArrayList("pageHistory");
-            if (restoredHistory != null) {
-                pageHistory.addAll(restoredHistory);
-            }
-        }
 
         FloatingActionButton floatingBtn = findViewById(R.id.floating_btn);
         ImageButton drawerToggle = findViewById(R.id.drawer_toggle);
@@ -164,15 +154,6 @@ public class MainActivity extends BaseActivity implements ActionButtonActivity {
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
 
-                if (pageHistory.isEmpty()) {
-                    pageHistory.push(0);
-                }
-
-                if (pageHistory.contains(tab.getPosition())) {
-                    pageHistory.remove(pageHistory.indexOf(tab.getPosition()));
-                }
-
-                pageHistory.push(tab.getPosition());
                 tintTab(tab, true);
             }
 
@@ -214,14 +195,6 @@ public class MainActivity extends BaseActivity implements ActionButtonActivity {
     }
 
     @Override
-    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
-        ArrayList<Integer> pageHistoryList = new ArrayList<>();
-        pageHistoryList.addAll(pageHistory);
-        outState.putIntegerArrayList("pageHistory", pageHistoryList);
-        super.onSaveInstanceState(outState, outPersistentState);
-    }
-
-    @Override
     protected void onResume() {
         super.onResume();
 
@@ -252,11 +225,10 @@ public class MainActivity extends BaseActivity implements ActionButtonActivity {
     public void onBackPressed() {
         if (drawer != null && drawer.isDrawerOpen()) {
             drawer.closeDrawer();
-        } else if (pageHistory.size() < 2) {
-            super.onBackPressed();
+        } else if (viewPager.getCurrentItem() != 0) {
+            viewPager.setCurrentItem(0);
         } else {
-            pageHistory.pop();
-            viewPager.setCurrentItem(pageHistory.peek());
+            super.onBackPressed();
         }
     }
 
@@ -441,7 +413,7 @@ public class MainActivity extends BaseActivity implements ActionButtonActivity {
 
         mastodonApi.accountVerifyCredentials().enqueue(new Callback<Account>() {
             @Override
-            public void onResponse(Call<Account> call, Response<Account> response) {
+            public void onResponse(@NonNull Call<Account> call, @NonNull Response<Account> response) {
                 if (response.isSuccessful()) {
                     onFetchUserInfoSuccess(response.body(), domain);
                 } else {
@@ -450,7 +422,7 @@ public class MainActivity extends BaseActivity implements ActionButtonActivity {
             }
 
             @Override
-            public void onFailure(Call<Account> call, Throwable t) {
+            public void onFailure(@NonNull Call<Account> call, @NonNull Throwable t) {
                 onFetchUserInfoFailure((Exception) t);
             }
         });
