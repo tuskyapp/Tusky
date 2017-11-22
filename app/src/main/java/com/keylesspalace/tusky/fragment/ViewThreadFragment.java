ba3b40d7e4352a65f26c4ab561fc1d7534819441
Conny Duck <k.pozniak@gmx.at>
2017-11-22 22:39:38 +0100

    fix color of thread line in light theme

--- app/src/main/java/com/keylesspalace/tusky/fragment/ViewThreadFragment.java
+++ app/src/main/java/com/keylesspalace/tusky/fragment/ViewThreadFragment.java
@@ -24,7 +24,6 @@ import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.DividerItemDecoration;
@@ -104,12 +103,15 @@ public class ViewThreadFragment extends SFragment implements
         recyclerView.setLayoutManager(layoutManager);
         DividerItemDecoration divider = new DividerItemDecoration(
                 context, layoutManager.getOrientation());
-        Drawable drawable = ThemeUtils.getDrawable(context, R.attr.status_divider_drawable,
+        Drawable dividerDrawable = ThemeUtils.getDrawable(context, R.attr.status_divider_drawable,
                 R.drawable.status_divider_dark);
-        divider.setDrawable(drawable);
+        divider.setDrawable(dividerDrawable);
         recyclerView.addItemDecoration(divider);
+
+        Drawable threadLineDrawable = ThemeUtils.getDrawable(context, R.attr.conversation_thread_line_drawable,
+                R.drawable.conversation_thread_line_dark);
         recyclerView.addItemDecoration(new ConversationLineItemDecoration(context,
-                ContextCompat.getDrawable(context, R.drawable.conversation_divider_dark)));
+                threadLineDrawable));
         adapter = new ThreadAdapter(this);
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(
                 getActivity());
