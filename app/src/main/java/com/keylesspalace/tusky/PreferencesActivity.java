494abe737e709ac77ce443b34b7f7f3803104898
remi6397 <remi6397@users.noreply.github.com>
2018-01-14 20:31:49 +0100

    Cleanup

--- app/src/main/java/com/keylesspalace/tusky/PreferencesActivity.java
+++ app/src/main/java/com/keylesspalace/tusky/PreferencesActivity.java
@@ -16,23 +16,18 @@
 package com.keylesspalace.tusky;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.annotation.XmlRes;
 import android.support.v13.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.keylesspalace.tusky.fragment.PreferencesFragment;
 import com.keylesspalace.tusky.util.ResourcesUtils;
