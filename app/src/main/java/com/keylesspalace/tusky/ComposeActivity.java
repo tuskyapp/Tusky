3c795c6eef4d8637ef67691053897a8e0fddab77
Conny Duck <k.pozniak@gmx.at>
2018-01-31 21:57:50 +0100

    upgrade to minSdkVersion 18, fix some lint warnings

--- app/src/main/java/com/keylesspalace/tusky/ComposeActivity.java
+++ app/src/main/java/com/keylesspalace/tusky/ComposeActivity.java
@@ -1030,8 +1030,7 @@ public final class ComposeActivity extends BaseActivity
     }
 
     private void onMediaPick() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN &&
-                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
                         != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
