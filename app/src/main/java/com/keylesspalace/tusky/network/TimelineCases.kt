0ce635f13eac5d9cd5192526f3789bf33e73cddc
charlag <ivan.kupalov@icloud.com>
2018-03-24 21:06:07 +0300

    Add missing licenses

--- app/src/main/java/com/keylesspalace/tusky/network/TimelineCases.kt
+++ app/src/main/java/com/keylesspalace/tusky/network/TimelineCases.kt
@@ -1,19 +1,29 @@
+/* Copyright 2018 charlag
+ *
+ * This file is a part of Tusky.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Tusky is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Tusky; if not,
+ * see <http://www.gnu.org/licenses>. */
+
 package com.keylesspalace.tusky.network
 
-import android.content.Context
 import android.content.Intent
 import android.support.v4.content.LocalBroadcastManager
 import com.keylesspalace.tusky.entity.Relationship
 import com.keylesspalace.tusky.entity.Status
 import com.keylesspalace.tusky.receiver.TimelineReceiver
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import android.support.annotation.NonNull
-import okhttp3.ResponseBody
-
-
-
 
 /**
  * Created by charlag on 3/24/18.
