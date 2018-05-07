8ffeee7e0f2764747ac83c6359e18d1c448c6bff
Jeremiasz Nelz <remi6397@gmail.com>
2018-05-07 19:50:55 +0200

    Start working on popup AuthorizeFollowActivity

--- app/src/main/java/com/keylesspalace/tusky/AuthorizeFollowActivity.kt
+++ app/src/main/java/com/keylesspalace/tusky/AuthorizeFollowActivity.kt
@@ -1,12 +1,103 @@
+/* Copyright 2018 Jeremiasz Nelz
+ *
+ * This file is a part of Tusky.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Tusky is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Tusky; if not,
+ * see <http://www.gnu.org/licenses>. */
+
 package com.keylesspalace.tusky
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
+import android.support.v7.widget.AppCompatButton
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import com.keylesspalace.tusky.adapter.FollowingAccountListAdapter
+import com.keylesspalace.tusky.adapter.OnFollowingAccountSelectedListener
+import com.keylesspalace.tusky.db.AccountEntity
+import com.keylesspalace.tusky.entity.Relationship
+import com.keylesspalace.tusky.network.MastodonApi
+import com.keylesspalace.tusky.receiver.TimelineReceiver
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import javax.inject.Inject
 
-class AuthorizeFollowActivity : AppCompatActivity() {
+class AuthorizeFollowActivity : BaseActivity(), OnFollowingAccountSelectedListener {
+
+    @Inject
+    lateinit var mastodonApi : MastodonApi
+
+    lateinit var recyclerView: RecyclerView
+    lateinit var cancelButton: AppCompatButton
+    lateinit var adapter: FollowingAccountListAdapter
+
+    private val followingAccounts: List<Pair<AccountEntity, FollowingAccountListAdapter.FollowState>> = emptyList()
+
+    private fun broadcast(action: String, id: String) {
+        val intent = Intent(action)
+        intent.putExtra("id", id)
+        LocalBroadcastManager.getInstance(applicationContext).sendBroadcast(intent)
+    }
+
+    override fun onFollowingAccountSelected(account: AccountEntity) {
+        var followState: FollowingAccountListAdapter.FollowState = FollowingAccountListAdapter.FollowState.NOT_FOLLOWING
+        val cb = object : Callback<Relationship> {
+
+            override fun onResponse(call: Call<Relationship>, response: Response<Relationship>) {
+                val relationship = response.body()
+                if (response.isSuccessful && relationship != null) {
+                    when {
+                        relationship.following -> followState = FollowingAccountListAdapter.FollowState.FOLLOWING
+                        relationship.requested -> followState = FollowingAccountListAdapter.FollowState.REQUESTED
+                        else -> {
+                            followState = FollowingAccountListAdapter.FollowState.NOT_FOLLOWING
+                            broadcast(TimelineReceiver.Types.UNFOLLOW_ACCOUNT, account.accountId)
+                        }
+                    }
+                } else {
+                }
+            }
+
+            override fun onFailure(call: Call<Relationship>?, t: Throwable?) {
+            }
+        }
+
+        followingAccounts.map { if (it.first == account) Pair(account, followState) else it }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authorize_follow)
+
+        recyclerView = findViewById(R.id.selection_frame)
+        cancelButton = findViewById(R.id.cancel_button)
+
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        adapter = FollowingAccountListAdapter(followingAccounts, this)
+        recyclerView.adapter = adapter
+
+        cancelButton.setOnClickListener({ finish() })
+
+        loadAccounts()
     }
+
+    private fun loadAccounts() {
+        for (account in accountManager.getAllAccountsOrderedByActive()) {
+            mastodonApi.followAccount()
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/keylesspalace/tusky/adapter/FollowingAccountListAdapter.kt
@@ -0,0 +1,83 @@
+/* Copyright 2018 Jeremiasz Nelz
+ *
+ * This file is a part of Tusky.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Tusky is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Tusky; if not,
+ * see <http://www.gnu.org/licenses>. */
+
+package com.keylesspalace.tusky.adapter
+
+import android.support.v7.widget.AppCompatButton
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.RelativeLayout
+import android.widget.TextView
+import com.keylesspalace.tusky.R
+import com.keylesspalace.tusky.adapter.FollowingAccountListAdapter.FollowState.*
+import com.keylesspalace.tusky.db.AccountEntity
+import com.pkmmte.view.CircularImageView
+import com.squareup.picasso.Picasso
+
+class FollowingAccountListAdapter(private val accountList: List<Pair<AccountEntity, FollowState>>,
+                                  private val onFollowingAccountSelectedListener: OnFollowingAccountSelectedListener)
+    : RecyclerView.Adapter<FollowingAccountListAdapter.FollowingAccountListViewHolder>() {
+
+    enum class FollowState {
+        NOT_FOLLOWING,
+        FOLLOWING,
+        REQUESTED
+    }
+
+    override fun getItemCount(): Int {
+        return accountList.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FollowingAccountListViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_following_account, parent, false) as RelativeLayout
+        return FollowingAccountListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: FollowingAccountListViewHolder, position: Int) {
+        val account = accountList[position].first
+        val followState = accountList[position].second
+
+        Picasso.with(holder.layout.context).load(account.profilePictureUrl)
+                .error(R.drawable.avatar_default)
+                .placeholder(R.drawable.avatar_default)
+                .into(holder.avatar)
+
+        holder.displayName.text = account.displayName
+        holder.username.text = account.username
+
+        holder.followButton.setText(when (followState) {
+            NOT_FOLLOWING -> R.string.action_follow
+            FOLLOWING -> R.string.action_unfollow
+            REQUESTED -> R.string.state_follow_requested
+        })
+
+        holder.followButton.setOnClickListener {
+            onFollowingAccountSelectedListener.onFollowingAccountSelected(account)
+        }
+    }
+
+    class FollowingAccountListViewHolder(val layout: RelativeLayout) : RecyclerView.ViewHolder(layout) {
+        val avatar: CircularImageView = layout.findViewById(R.id.account_avatar)
+        val displayName: TextView = layout.findViewById(R.id.account_display_name)
+        val username: TextView = layout.findViewById(R.id.account_username)
+        val followButton: AppCompatButton = layout.findViewById(R.id.follow_btn)
+    }
+
+}
+
+interface OnFollowingAccountSelectedListener {
+    fun onFollowingAccountSelected(account: AccountEntity)
+}
