51b4c2f7480fcfcdc11a5d52b69ffbc3a4483ee9
Conny Duck <k.pozniak@gmx.at>
2018-06-24 09:52:17 +0200

    new License activity

--- app/src/main/java/com/keylesspalace/tusky/LicenseActivity.kt
+++ app/src/main/java/com/keylesspalace/tusky/LicenseActivity.kt
@@ -1,12 +1,37 @@
+/* Copyright 2018 Conny Duck
+ *
+ * This file is a part of Tusky.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Tusky is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Tusky; if not,
+ * see <http://www.gnu.org/licenses>. */
+
 package com.keylesspalace.tusky
 
 import android.os.Bundle
+import android.support.annotation.RawRes
+import android.util.Log
 import android.view.MenuItem
-import com.keylesspalace.tusky.di.Injectable
+import android.widget.TextView
+import com.keylesspalace.tusky.util.IOUtils
+import kotlinx.android.extensions.CacheImplementation
+import kotlinx.android.extensions.ContainerOptions
+import kotlinx.android.synthetic.main.activity_license.*
 import kotlinx.android.synthetic.main.toolbar_basic.*
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
 
 class LicenseActivity : BaseActivity() {
 
+    @ContainerOptions(cache = CacheImplementation.NO_CACHE)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_license)
@@ -18,6 +43,8 @@ class LicenseActivity : BaseActivity() {
         }
 
         setTitle(R.string.title_licenses)
+
+        loadFileIntoTextView(R.raw.apache, licenseApacheTextView)
 
     }
 
@@ -31,5 +58,27 @@ class LicenseActivity : BaseActivity() {
         return super.onOptionsItemSelected(item)
     }
 
+    private fun loadFileIntoTextView(@RawRes fileId: Int, textView: TextView) {
+
+        val sb = StringBuilder()
+
+        val br = BufferedReader(InputStreamReader(resources.openRawResource(fileId)))
+
+        try {
+            var line: String? = br.readLine()
+            while (line != null) {
+                sb.append(line)
+                sb.append('\n')
+                line = br.readLine()
+            }
+        } catch (e: IOException) {
+            Log.w("LicenseActivity", e)
+        }
+
+        IOUtils.closeQuietly(br)
+
+        textView.text = sb.toString()
+
+    }
 
 }
