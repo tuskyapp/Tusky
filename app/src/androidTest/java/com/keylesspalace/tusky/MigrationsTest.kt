f7bc954f37c430c7f6446069c2cafa3fa278e149
charlag <ivan.kupalov@icloud.com>
2018-10-12 20:52:05 +0200

    Fix migration, add 8-9 migration test

---
+++ app/src/androidTest/java/com/keylesspalace/tusky/MigrationsTest.kt
@@ -0,0 +1,61 @@
+package com.keylesspalace.tusky
+
+import android.arch.persistence.db.framework.FrameworkSQLiteOpenHelperFactory
+import android.arch.persistence.room.testing.MigrationTestHelper
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import com.keylesspalace.tusky.db.AppDatabase
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+const val TEST_DB = "mirgation_test"
+
+@RunWith(AndroidJUnit4::class)
+class MigrationsTest {
+
+    @JvmField
+    @Rule
+    var helper: MigrationTestHelper = MigrationTestHelper(
+            InstrumentationRegistry.getInstrumentation(),
+            AppDatabase::class.java.canonicalName,
+            FrameworkSQLiteOpenHelperFactory()
+    )
+
+    @Test
+    fun migrateTo9() {
+        val db = helper.createDatabase(TEST_DB, 8)
+
+        val id = 1
+        val domain = "domain.site"
+        val token = "token"
+        val active = true
+        val accountId = "accountId"
+        val username = "username"
+        val values = arrayOf(id, domain, token, active, accountId, username, "Display Name",
+                "https://picture.url", true, true, true, true, true, true, true,
+                true, "1000", "[]", "[{\"shortcode\": \"emoji\", \"url\": \"yes\"}]")
+
+        db.execSQL("INSERT OR REPLACE INTO `AccountEntity`(`id`,`domain`,`accessToken`,`isActive`," +
+                "`accountId`,`username`,`displayName`,`profilePictureUrl`,`notificationsEnabled`," +
+                "`notificationsMentioned`,`notificationsFollowed`,`notificationsReblogged`," +
+                "`notificationsFavorited`,`notificationSound`,`notificationVibration`," +
+                "`notificationLight`,`lastNotificationId`,`activeNotifications`,`emojis`) " +
+                "VALUES (nullif(?, 0),?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+                values)
+
+        db.close()
+
+        val newDb = helper.runMigrationsAndValidate(TEST_DB, 9, true, AppDatabase.MIGRATION_8_9)
+
+        val cursor = newDb.query("SELECT * FROM AccountEntity")
+        cursor.moveToFirst()
+        assertEquals(id, cursor.getInt(0))
+        assertEquals(domain, cursor.getString(1))
+        assertEquals(token, cursor.getString(2))
+        assertEquals(active, cursor.getInt(3) != 0)
+        assertEquals(accountId, cursor.getString(4))
+        assertEquals(username, cursor.getString(5))
+    }
+}
