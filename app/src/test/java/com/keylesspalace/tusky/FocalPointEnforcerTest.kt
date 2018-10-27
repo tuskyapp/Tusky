cece4aa0a554a07bdab15214e9259671b12238e3
Jochem Raat <jchmrt@riseup.net>
2018-10-27 18:16:48 +0200

    Use static imports to remove clutter in FocalPointEnforcerTest
    Instead of duplication Assert. in front of every assertEquals, simply
    statically import it.

--- app/src/test/java/com/keylesspalace/tusky/FocalPointEnforcerTest.kt
+++ app/src/test/java/com/keylesspalace/tusky/FocalPointEnforcerTest.kt
@@ -16,7 +16,9 @@
 package com.keylesspalace.tusky
 
 import com.keylesspalace.tusky.util.FocalPointEnforcer
-import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Assert.assertFalse
 import org.junit.Test
 
 class FocalPointEnforcerTest {
@@ -25,30 +27,30 @@ class FocalPointEnforcerTest {
     // focal[X|Y]ToCoordinate tests
     @Test
     fun positiveFocalXToCoordinateTest() {
-        Assert.assertEquals(FocalPointEnforcer.focalXToCoordinate(0.4f), 0.7f, eps)
+        assertEquals(FocalPointEnforcer.focalXToCoordinate(0.4f), 0.7f, eps)
     }
     @Test
     fun negativeFocalXToCoordinateTest() {
-        Assert.assertEquals(FocalPointEnforcer.focalXToCoordinate(-0.8f), 0.1f, eps)
+        assertEquals(FocalPointEnforcer.focalXToCoordinate(-0.8f), 0.1f, eps)
     }
     @Test
     fun positiveFocalYToCoordinateTest() {
-        Assert.assertEquals(FocalPointEnforcer.focalYToCoordinate(-0.2f), 0.6f, eps)
+        assertEquals(FocalPointEnforcer.focalYToCoordinate(-0.2f), 0.6f, eps)
     }
     @Test
     fun negativeFocalYToCoordinateTest() {
-        Assert.assertEquals(FocalPointEnforcer.focalYToCoordinate(0.0f), 0.5f, eps)
+        assertEquals(FocalPointEnforcer.focalYToCoordinate(0.0f), 0.5f, eps)
     }
 
     // isVerticalCrop tests
     @Test
     fun isVerticalCropTest() {
-        Assert.assertTrue(FocalPointEnforcer.isVerticalCrop(2f, 1f,
+        assertTrue(FocalPointEnforcer.isVerticalCrop(2f, 1f,
                 1f, 2f))
     }
     @Test
     fun isHorizontalCropTest() {
-        Assert.assertFalse(FocalPointEnforcer.isVerticalCrop(1f, 2f,
+        assertFalse(FocalPointEnforcer.isVerticalCrop(1f, 2f,
                 2f,1f))
     }
     @Test
@@ -60,49 +62,49 @@ class FocalPointEnforcerTest {
     // calculateScaling tests
     @Test
     fun perfectFitScaleDownTest() {
-        Assert.assertEquals(FocalPointEnforcer.calculateScaling(2f, 5f,
+        assertEquals(FocalPointEnforcer.calculateScaling(2f, 5f,
                 5f, 12.5f), 0.4f, eps)
     }
     @Test
     fun perfectFitScaleUpTest() {
-        Assert.assertEquals(FocalPointEnforcer.calculateScaling(2f, 5f,
+        assertEquals(FocalPointEnforcer.calculateScaling(2f, 5f,
                 1f, 2.5f), 2f, eps)
     }
     @Test
     fun verticalCropScaleUpTest() {
-        Assert.assertEquals(FocalPointEnforcer.calculateScaling(2f, 1f,
+        assertEquals(FocalPointEnforcer.calculateScaling(2f, 1f,
                 1f, 2f), 2f, eps)
     }
     @Test
     fun verticalCropScaleDownTest() {
-        Assert.assertEquals(FocalPointEnforcer.calculateScaling(4f, 3f,
+        assertEquals(FocalPointEnforcer.calculateScaling(4f, 3f,
                 8f, 24f), 0.5f, eps)
     }
     @Test
     fun horizontalCropScaleUpTest() {
-        Assert.assertEquals(FocalPointEnforcer.calculateScaling(1f, 2f,
+        assertEquals(FocalPointEnforcer.calculateScaling(1f, 2f,
                 2f, 1f), 2f, eps)
     }
     @Test
     fun horizontalCropScaleDownTest() {
-        Assert.assertEquals(FocalPointEnforcer.calculateScaling(3f, 4f,
+        assertEquals(FocalPointEnforcer.calculateScaling(3f, 4f,
                 24f, 8f), 0.5f, eps)
     }
 
     // focalOffset tests
     @Test
     fun toLowFocalOffsetTest() {
-        Assert.assertEquals(FocalPointEnforcer.focalOffset(2f, 8f, 1f, 0.05f),
+        assertEquals(FocalPointEnforcer.focalOffset(2f, 8f, 1f, 0.05f),
                 0f, eps)
     }
     @Test
     fun toHighFocalOffsetTest() {
-        Assert.assertEquals(FocalPointEnforcer.focalOffset(2f, 4f, 2f,0.95f),
+        assertEquals(FocalPointEnforcer.focalOffset(2f, 4f, 2f,0.95f),
                 -6f, eps)
     }
     @Test
     fun possibleFocalOffsetTest() {
-        Assert.assertEquals(FocalPointEnforcer.focalOffset(2f, 4f, 2f,0.7f),
+        assertEquals(FocalPointEnforcer.focalOffset(2f, 4f, 2f,0.7f),
                 -4.6f, eps)
     }
 }
