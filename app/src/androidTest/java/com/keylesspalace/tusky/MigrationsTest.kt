6612d8bcf8f41d26561b2855406f04be2cf850a9
charlag <ivan.kupalov@icloud.com>
2018-12-26 15:19:38 +0100

    Fix tests

--- app/src/androidTest/java/com/keylesspalace/tusky/MigrationsTest.kt
+++ app/src/androidTest/java/com/keylesspalace/tusky/MigrationsTest.kt
@@ -2,8 +2,8 @@ package com.keylesspalace.tusky
 
 import androidx.room.testing.MigrationTestHelper
 import androidx.sqlite.db.framework.FrameworkSQLiteOpenHelperFactory
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.runner.AndroidJUnit4
 import com.keylesspalace.tusky.db.AppDatabase
 import org.junit.Assert.assertEquals
 import org.junit.Rule
@@ -24,7 +24,7 @@ class MigrationsTest {
     )
 
     @Test
-    fun migrateTo9() {
+    fun migrateTo11() {
         val db = helper.createDatabase(TEST_DB, 10)
 
         val id = 1
@@ -35,14 +35,17 @@ class MigrationsTest {
         val username = "username"
         val values = arrayOf(id, domain, token, active, accountId, username, "Display Name",
                 "https://picture.url", true, true, true, true, true, true, true,
-                true, "1000", "[]", "[{\"shortcode\": \"emoji\", \"url\": \"yes\"}]")
+                true, "1000", "[]", "[{\"shortcode\": \"emoji\", \"url\": \"yes\"}]", 0, false,
+                false, true)
 
         db.execSQL("INSERT OR REPLACE INTO `AccountEntity`(`id`,`domain`,`accessToken`,`isActive`," +
                 "`accountId`,`username`,`displayName`,`profilePictureUrl`,`notificationsEnabled`," +
                 "`notificationsMentioned`,`notificationsFollowed`,`notificationsReblogged`," +
                 "`notificationsFavorited`,`notificationSound`,`notificationVibration`," +
-                "`notificationLight`,`lastNotificationId`,`activeNotifications`,`emojis`) " +
-                "VALUES (nullif(?, 0),?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+                "`notificationLight`,`lastNotificationId`,`activeNotifications`,`emojis`," +
+                "`defaultPostPrivacy`,`defaultMediaSensitivity`,`alwaysShowSensitiveMedia`," +
+                "`mediaPreviewEnabled`) " +
+                "VALUES (nullif(?, 0),?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
                 values)
 
         db.close()
