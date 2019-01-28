ce80b0c663eb507d77f1c7c49ab216d7e7f049e7
Ivan Kupalov <charlag@tutanota.com>
2019-01-28 11:05:13 +0100

    Fix crash in media tab, fix #1005 (#1007)

--- app/src/main/java/com/keylesspalace/tusky/di/AppInjector.kt
+++ app/src/main/java/com/keylesspalace/tusky/di/AppInjector.kt
@@ -17,6 +17,7 @@ package com.keylesspalace.tusky.di
 
 import android.app.Activity
 import android.app.Application
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
@@ -68,8 +69,7 @@ object AppInjector {
         if (activity is FragmentActivity) {
             activity.supportFragmentManager.registerFragmentLifecycleCallbacks(
                     object : FragmentManager.FragmentLifecycleCallbacks() {
-                        override fun onFragmentCreated(fm: FragmentManager, f: Fragment,
-                                                       savedInstanceState: Bundle?) {
+                        override fun onFragmentPreAttached(fm: FragmentManager, f: Fragment, context: Context) {
                             if (f is Injectable) {
                                 AndroidSupportInjection.inject(f)
                             }
