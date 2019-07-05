bc956240e3d13af318c4df054040428d061f7c2c
Matt Fenlon <nolnefm@gmail.com>
2019-07-05 19:18:38 +0100

    Remove unnecessary import

--- app/src/main/java/com/keylesspalace/tusky/adapter/StatusBaseViewHolder.java
+++ app/src/main/java/com/keylesspalace/tusky/adapter/StatusBaseViewHolder.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.text.Spanned;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
