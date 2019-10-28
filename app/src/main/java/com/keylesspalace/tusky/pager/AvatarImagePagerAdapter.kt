06b5c1b553e9ad662e933e7cfee78778cd3469d5
Conny Duck <k.pozniak@gmx.at>
2019-10-28 19:27:13 +0100

    migrate ViewMediaActivty to ViewPager2

--- app/src/main/java/com/keylesspalace/tusky/pager/AvatarImagePagerAdapter.kt
+++ app/src/main/java/com/keylesspalace/tusky/pager/AvatarImagePagerAdapter.kt
@@ -1,13 +1,16 @@
 package com.keylesspalace.tusky.pager
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
-import com.keylesspalace.tusky.SharedElementTransitionListener
+import androidx.fragment.app.FragmentActivity
+import com.keylesspalace.tusky.ViewMediaAdapter
 import com.keylesspalace.tusky.fragment.ViewMediaFragment
 
-class AvatarImagePagerAdapter(fragmentManager: FragmentManager, private val avatarUrl: String) : FragmentPagerAdapter(fragmentManager), SharedElementTransitionListener {
-    override fun getItem(position: Int): Fragment {
+class AvatarImagePagerAdapter(
+        activity: FragmentActivity,
+        private val avatarUrl: String
+) : ViewMediaAdapter(activity) {
+
+    override fun createFragment(position: Int): Fragment {
         return if (position == 0) {
             ViewMediaFragment.newAvatarInstance(avatarUrl)
         } else {
@@ -15,8 +18,8 @@ class AvatarImagePagerAdapter(fragmentManager: FragmentManager, private val avat
         }
     }
 
-    override fun getCount() = 1
+    override fun getItemCount() = 1
 
-    override fun onTransitionEnd() {
+    override fun onTransitionEnd(position: Int) {
     }
 }
