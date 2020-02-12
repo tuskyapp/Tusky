2aa3b1aed9fbd680de050775f5a4a03c7eef8346
Levi Bard <taktaktaktaktaktaktaktaktaktak@gmail.com>
2020-02-12 09:29:18 +0100

    Clean up string management

--- app/src/main/java/com/keylesspalace/tusky/util/TesseractHelper.kt
+++ app/src/main/java/com/keylesspalace/tusky/util/TesseractHelper.kt
@@ -30,11 +30,11 @@ class TesseractHelper {
                 try {
                     // FIXME: Offer language selection / training data download? Ship training data with translations?
                     if (!tesseractApi.init(getDataDirectory(context), iso3Language, TessBaseAPI.OEM_TESSERACT_ONLY)) {
-                        throw RuntimeException("Unknown error")
+                        throw RuntimeException(context.getString(R.string.error_generic))
                     }
                     tesseractLanguage = iso3Language
                 } catch (e: Exception) {
-                    Toast.makeText(context, String.format("Error initializing OCR engine: %s", e), Toast.LENGTH_LONG).show()
+                    Toast.makeText(context, context.getString(R.string.training_data_initialization_error, e), Toast.LENGTH_LONG).show()
                 }
             }
         }
@@ -53,17 +53,17 @@ class TesseractHelper {
                     input.setText(text)
                 }
             } catch (e: Exception) {
-                Toast.makeText(context, String.format("Error performing OCR: %s", e), Toast.LENGTH_LONG).show()
+                Toast.makeText(context, context.getString(R.string.training_data_ocr_error, e), Toast.LENGTH_LONG).show()
             }
         }
 
         private fun downloadTrainingData(context: Context, language: String) {
-            val baseFilename = String.format("%s.traineddata", language)
+            val baseFilename = "${language}.traineddata"
             // The com.rmtheis package is still using tesseract 3.x
             // The newer version at https://github.com/alexcohn/tess-two supports tesseract 4.x
             // (which has much smaller training data), but we'd have to compile it ourselves?
-            val uri = Uri.parse(String.format("https://github.com/tesseract-ocr/tessdata/raw/3.04.00/%s", baseFilename));
-            val localPath = File(getDataDirectory(context), String.format("tessdata/%s", baseFilename)).toUri()
+            val uri = Uri.parse("https://github.com/tesseract-ocr/tessdata/raw/3.04.00/$baseFilename")
+            val localPath = File(getDataDirectory(context), "tessdata/$baseFilename").toUri()
             val downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
             downloadManager.enqueue(DownloadManager.Request(uri)
                     .setDestinationUri(localPath)
@@ -75,7 +75,7 @@ class TesseractHelper {
             val language = TuskyApplication.localeManager.getISO3Locale()
             downloadTrainingData(context, language)
             if (languagesWithVerticalData.contains(language)) {
-                downloadTrainingData(context, String.format("%s_vert", language))
+                downloadTrainingData(context, "${language}_vert")
             }
         }
     }
