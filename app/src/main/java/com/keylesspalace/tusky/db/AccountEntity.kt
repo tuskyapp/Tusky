0bc89b0a936307419eacec9d2b5b1c5eb22278a6
Levi Bard <taktaktaktaktaktaktaktaktaktak@gmail.com>
2020-03-18 12:45:57 +0100

    Filter follow request notifications by default

--- app/src/main/java/com/keylesspalace/tusky/db/AccountEntity.kt
+++ app/src/main/java/com/keylesspalace/tusky/db/AccountEntity.kt
@@ -55,7 +55,7 @@ data class AccountEntity(@field:PrimaryKey(autoGenerate = true) var id: Long,
                          var activeNotifications: String = "[]",
                          var emojis: List<Emoji> = emptyList(),
                          var tabPreferences: List<TabData> = defaultTabs(),
-                         var notificationsFilter: String = "[]") {
+                         var notificationsFilter: String = "[\"follow_request\"]") {
 
     val identifier: String
         get() = "$domain:$accountId"
