johanw666/Gab

Add preference to disable swipe gesture for switching tabs, close #1718

app/src/main/java/com/keylesspalace/tusky/MainActivity.kt
@@ -167,6 +167,11 @@ class MainActivity : BottomSheetActivity(), ActionButtonActivity, HasAndroidInje
 
         val pageMargin = resources.getDimensionPixelSize(R.dimen.tab_page_margin)
         viewPager.setPageTransformer(MarginPageTransformer(pageMargin))
+
+        val uswSwipeForTabs = PreferenceManager.getDefaultSharedPreferences(this)
+                .getBoolean("uswSwipeForTabs", true)
+        viewPager.isUserInputEnabled = uswSwipeForTabs
+
         tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
                 if (tab.position == notificationTabPosition) {

tuskyapp/Tusky

app/src/main/java/com/keylesspalace/tusky/MainActivity.kt
@@ -168,6 +168,11 @@ class MainActivity : BottomSheetActivity(), ActionButtonActivity, HasAndroidInje
 
         val pageMargin = resources.getDimensionPixelSize(R.dimen.tab_page_margin)
         viewPager.setPageTransformer(MarginPageTransformer(pageMargin))
+
+        val uswSwipeForTabs = PreferenceManager.getDefaultSharedPreferences(this)
+                .getBoolean("enableSwipeForTabs", true)
+        viewPager.isUserInputEnabled = uswSwipeForTabs
+
         tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
                 if (tab.position == notificationTabPosition) {