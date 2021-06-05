526324b55eb51436e9b8c6721daff9b31f24cfd6
Conny Duck <k.pozniak@gmx.at>
2021-06-05 13:15:42 +0200

    remove mastodon bug workaround

--- app/src/main/java/com/keylesspalace/tusky/components/search/adapter/SearchPagingSource.kt
+++ app/src/main/java/com/keylesspalace/tusky/components/search/adapter/SearchPagingSource.kt
@@ -63,21 +63,7 @@ class SearchPagingSource<T: Any>(
                 following = false
             ).await()
 
-            // Working around Mastodon bug where exact match is returned no matter
-            // which offset is requested (so if we search for a full username, it's
-            // infinite)
-            // see https://github.com/tootsuite/mastodon/issues/11365
-            // see https://github.com/tootsuite/mastodon/issues/13083
-            val res = if ((data.accounts.size == 1 && data.accounts[0].username.equals(
-                    searchRequest,
-                    ignoreCase = true
-                ))
-                || (data.statuses.size == 1 && data.statuses[0].url.equals(searchRequest))
-            ) {
-                listOf()
-            } else {
-                parser(data)
-            }
+            val res = parser(data)
 
             val nextKey = if (res.isEmpty()) {
                 null
