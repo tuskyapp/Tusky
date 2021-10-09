d1d564a7e23afb4c9db49073097b5f7175ce946e
Konrad Pozniak <connyduck@users.noreply.github.com>
2021-10-09 10:45:41 +0200

    support animated PNGs and WEBPs (#2244)

---
+++ app/src/main/java/com/keylesspalace/tusky/util/GlideModule.kt
@@ -0,0 +1,7 @@
+package com.keylesspalace.tusky.util
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class GlideModule : AppGlideModule()
