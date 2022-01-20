70595512101a657a3bad0185bb7d6eb62370e5f3
Konrad Pozniak <k.pozniak@gmx.at>
2022-01-20 20:59:48 +0100

    use custom JsonAdapter to be compatible with instances that have a custom subscribing feature

---
+++ app/src/main/java/com/keylesspalace/tusky/json/GuardedBooleanAdapter.kt
@@ -0,0 +1,33 @@
+/* Copyright 2022 Tusky Contributors
+ *
+ * This file is a part of Tusky.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Tusky is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Tusky; if not,
+ * see <http://www.gnu.org/licenses>. */
+
+package com.keylesspalace.tusky.json
+
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.JsonParseException
+import java.lang.reflect.Type
+
+class GuardedBooleanAdapter : JsonDeserializer<Boolean?> {
+    @Throws(JsonParseException::class)
+    override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): Boolean? {
+        return if (json.isJsonObject) {
+            null
+        } else {
+            json.asBoolean
+        }
+    }
+}
