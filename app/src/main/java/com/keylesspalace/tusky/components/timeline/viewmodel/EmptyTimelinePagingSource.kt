9b5061e28ade8e67fc4d9ffd42891ac103f0500c
Conny Duck <k.pozniak@gmx.at>
2022-03-04 21:30:23 +0100

    fix ktlint

--- app/src/main/java/com/keylesspalace/tusky/components/timeline/viewmodel/EmptyTimelinePagingSource.kt
+++ app/src/main/java/com/keylesspalace/tusky/components/timeline/viewmodel/EmptyTimelinePagingSource.kt
@@ -4,7 +4,7 @@ import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.keylesspalace.tusky.db.TimelineStatusWithAccount
 
-class EmptyTimelinePagingSource: PagingSource<Int, TimelineStatusWithAccount>() {
+class EmptyTimelinePagingSource : PagingSource<Int, TimelineStatusWithAccount>() {
     override fun getRefreshKey(state: PagingState<Int, TimelineStatusWithAccount>): Int? = null
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, TimelineStatusWithAccount> = LoadResult.Page(emptyList(), null, null)
