e753bef7d9cad19f8c4cc73e2496d609538dbcdb
Konrad Pozniak <k.pozniak@gmx.at>
2022-04-15 21:11:39 +0200

    fix notifications showing unparsed html

--- app/src/main/java/com/keylesspalace/tusky/util/StatusParsingHelper.kt
+++ app/src/main/java/com/keylesspalace/tusky/util/StatusParsingHelper.kt
@@ -13,6 +13,7 @@
  * You should have received a copy of the GNU General Public License along with Tusky; if not,
  * see <http://www.gnu.org/licenses>. */
 
+@file:JvmName("StatusParsingHelper")
 package com.keylesspalace.tusky.util
 
 import android.text.SpannableStringBuilder
