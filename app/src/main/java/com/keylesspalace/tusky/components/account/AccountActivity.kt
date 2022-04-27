0e9d3623514a8234ed148a9648f9227dbb52967f
moddedBear <57692663+moddedBear@users.noreply.github.com>
2022-04-27 11:58:39 -0600

    Remove Identity Proof Support (#2456)
    * Remove IdentityProof.kt and refactor
    
    * Remove accountFieldData from viewmodel
    
    * Remove unused imports

--- app/src/main/java/com/keylesspalace/tusky/components/account/AccountActivity.kt
+++ app/src/main/java/com/keylesspalace/tusky/components/account/AccountActivity.kt
@@ -372,12 +372,6 @@ class AccountActivity : BottomSheetActivity(), ActionButtonActivity, HasAndroidI
                     .show()
             }
         }
-        viewModel.accountFieldData.observe(
-            this
-        ) {
-            accountFieldAdapter.fields = it
-            accountFieldAdapter.notifyDataSetChanged()
-        }
         viewModel.noteSaved.observe(this) {
             binding.saveNoteInfo.visible(it, View.INVISIBLE)
         }
@@ -409,7 +403,7 @@ class AccountActivity : BottomSheetActivity(), ActionButtonActivity, HasAndroidI
         val emojifiedNote = account.note.parseAsMastodonHtml().emojify(account.emojis, binding.accountNoteTextView, animateEmojis)
         setClickableText(binding.accountNoteTextView, emojifiedNote, emptyList(), null, this)
 
-        // accountFieldAdapter.fields = account.fields ?: emptyList()
+        accountFieldAdapter.fields = account.fields ?: emptyList()
         accountFieldAdapter.emojis = account.emojis ?: emptyList()
         accountFieldAdapter.notifyDataSetChanged()
 
