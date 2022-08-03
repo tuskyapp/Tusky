354fdd611c2b50a0a8d0bc4ef0b8827d2aca419c
Konrad Pozniak <k.pozniak@gmx.at>
2022-08-03 18:52:11 +0200

    update AndroidX dependencies

--- app/src/main/java/com/keylesspalace/tusky/receiver/SendStatusBroadcastReceiver.kt
+++ app/src/main/java/com/keylesspalace/tusky/receiver/SendStatusBroadcastReceiver.kt
@@ -135,6 +135,6 @@ class SendStatusBroadcastReceiver : BroadcastReceiver() {
     private fun getReplyMessage(intent: Intent): CharSequence {
         val remoteInput = RemoteInput.getResultsFromIntent(intent)
 
-        return remoteInput.getCharSequence(NotificationHelper.KEY_REPLY, "")
+        return remoteInput?.getCharSequence(NotificationHelper.KEY_REPLY, "") ?: ""
     }
 }
