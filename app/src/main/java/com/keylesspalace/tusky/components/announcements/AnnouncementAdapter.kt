a64dee9be00ae95a89befe4005e588b54a524e42
Benjamin Stürmer <webmaster@stuermer-benjamin.de>
2022-11-07 20:10:49 +0100

    #2072: hide add reaction when limit is reached (#2766)
    * #2072: hide add reaction when limit is reached
    
    * use extensions function for show/hide view
    
    * fix condition :(

--- app/src/main/java/com/keylesspalace/tusky/components/announcements/AnnouncementAdapter.kt
+++ app/src/main/java/com/keylesspalace/tusky/components/announcements/AnnouncementAdapter.kt
@@ -34,6 +34,7 @@ import com.keylesspalace.tusky.util.EmojiSpan
 import com.keylesspalace.tusky.util.emojify
 import com.keylesspalace.tusky.util.parseAsMastodonHtml
 import com.keylesspalace.tusky.util.setClickableText
+import com.keylesspalace.tusky.util.visible
 import java.lang.ref.WeakReference
 
 interface AnnouncementActionListener : LinkListener {
@@ -72,6 +73,9 @@ class AnnouncementAdapter(
             addReactionChip.visibility = View.GONE
             return
         }
+
+        // hide button if announcement badge limit is already reached
+        addReactionChip.visible(item.reactions.size < 8)
 
         item.reactions.forEachIndexed { i, reaction ->
             (
