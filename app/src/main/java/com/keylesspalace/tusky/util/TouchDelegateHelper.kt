9a0148fbb13f8fc4ee3472e87f9a5f26c90b07d4
Konrad Pozniak <k.pozniak@gmx.at>
2022-12-06 21:17:16 +0100

    fix crash in TouchDelegateHelper when not all views are available

--- app/src/main/java/com/keylesspalace/tusky/util/TouchDelegateHelper.kt
+++ app/src/main/java/com/keylesspalace/tusky/util/TouchDelegateHelper.kt
@@ -1,3 +1,18 @@
+/* Copyright 2022 Tusky contributors
+
+ * This file is a part of Tusky.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Tusky is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Tusky; if not,
+ * see <http://www.gnu.org/licenses>. */
+
 @file:JvmName("TouchDelegateHelper")
 
 package com.keylesspalace.tusky.util
@@ -12,25 +27,26 @@ import android.view.ViewGroup
  * Expands the touch area of the give row of views to fill the space in-between them, using a
  * [TouchDelegate].
  */
-fun ViewGroup.expandTouchSizeToFillRow(children: List<View>) {
+fun ViewGroup.expandTouchSizeToFillRow(children: List<View?>) {
     addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
         touchDelegate = CompositeTouchDelegate(
             this,
-            children.mapIndexed { i, view ->
-                val rect = Rect()
-                view.getHitRect(rect)
-                val left = children.getOrNull(i - 1)
-                if (left != null) {
-                    // extend half-way to previous view
-                    rect.left -= (view.left - left.right) / 2
+            children.filterNotNull()
+                .mapIndexed { i, view ->
+                    val rect = Rect()
+                    view.getHitRect(rect)
+                    val left = children.getOrNull(i - 1)
+                    if (left != null) {
+                        // extend half-way to previous view
+                        rect.left -= (view.left - left.right) / 2
+                    }
+                    val right = children.getOrNull(i + 1)
+                    if (right != null) {
+                        // extend half-way to next view
+                        rect.right += (right.left - view.right) / 2
+                    }
+                    TouchDelegate(rect, view)
                 }
-                val right = children.getOrNull(i + 1)
-                if (right != null) {
-                    // extend half-way to next view
-                    rect.right += (right.left - view.right) / 2
-                }
-                TouchDelegate(rect, view)
-            }
         )
     }
 }
@@ -39,13 +55,11 @@ private class CompositeTouchDelegate(view: View, private val delegates: List<Tou
     TouchDelegate(Rect(), view) {
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
-        var res = false
         val x = event.x
         val y = event.y
-        for (delegate in delegates) {
+        return delegates.fold(false) { res, delegate ->
             event.setLocation(x, y)
-            res = delegate.onTouchEvent(event) || res
+            delegate.onTouchEvent(event) || res
         }
-        return res
     }
 }
