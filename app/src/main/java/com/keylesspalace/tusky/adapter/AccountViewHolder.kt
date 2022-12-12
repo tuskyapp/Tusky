0270d9426cf8d6e34a79cd87f8e9623ab35de5ec
Nik Clayton <nik@ngo.org.uk>
2022-12-12 22:12:02 +0100

    Convert AccountViewHolder from Java to Kotlin
    Use view binding in the converted code, which requires small changes in code
    that calls constructors.
    
    Pass showBotOverlays as a parameter, rather than having the code reach in to
    the shared preferences, fixing a layering violation. This affects callers
    and classes derived from AccountAdapter.

---
+++ app/src/main/java/com/keylesspalace/tusky/adapter/AccountViewHolder.kt
@@ -0,0 +1,60 @@
+package com.keylesspalace.tusky.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.keylesspalace.tusky.R
+import com.keylesspalace.tusky.databinding.ItemAccountBinding
+import com.keylesspalace.tusky.entity.TimelineAccount
+import com.keylesspalace.tusky.interfaces.AccountActionListener
+import com.keylesspalace.tusky.interfaces.LinkListener
+import com.keylesspalace.tusky.util.emojify
+import com.keylesspalace.tusky.util.loadAvatar
+
+class AccountViewHolder(
+    private val binding: ItemAccountBinding
+) : RecyclerView.ViewHolder(binding.root) {
+    private var accountId: String? = null
+
+    fun setupWithAccount(
+        account: TimelineAccount,
+        animateAvatar: Boolean,
+        animateEmojis: Boolean,
+        showBotOverlay: Boolean
+    ) {
+        accountId = account.id
+
+        val format = binding.accountUsername.context.getString(R.string.post_username_format)
+        val formattedUsername = String.format(format, account.username)
+        binding.accountUsername.text = formattedUsername
+
+        val emojifiedName = account.name.emojify(
+            account.emojis,
+            binding.accountDisplayName,
+            animateEmojis
+        )
+        binding.accountDisplayName.text = emojifiedName
+
+        val avatarRadius = binding.accountAvatar.context.resources
+            .getDimensionPixelSize(R.dimen.avatar_radius_48dp)
+        loadAvatar(account.avatar, binding.accountAvatar, avatarRadius, animateAvatar)
+
+        if (showBotOverlay && account.bot) {
+            binding.accountAvatarInset.visibility = View.VISIBLE
+            binding.accountAvatarInset.setImageResource(R.drawable.bot_badge)
+        } else {
+            binding.accountAvatarInset.visibility = View.GONE
+        }
+    }
+
+    fun setupActionListener(listener: AccountActionListener) {
+        itemView.setOnClickListener { listener.onViewAccount(accountId) }
+    }
+
+    fun setupLinkListener(listener: LinkListener) {
+        itemView.setOnClickListener {
+            listener.onViewAccount(
+                accountId!!
+            )
+        }
+    }
+}
