12be70991aeda0b7e14a7b830a4a68aaf88ffd07
Nik Clayton <nik@ngo.org.uk>
2022-12-24 22:41:03 +0100

    Remove unnecessary import

--- app/src/main/java/com/keylesspalace/tusky/components/scheduled/ScheduledStatusAdapter.kt
+++ app/src/main/java/com/keylesspalace/tusky/components/scheduled/ScheduledStatusAdapter.kt
@@ -16,7 +16,6 @@
 package com.keylesspalace.tusky.components.scheduled
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
