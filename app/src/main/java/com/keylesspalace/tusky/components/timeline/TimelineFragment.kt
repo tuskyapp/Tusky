5eebe05ebcbfbcac20146bb1200523487c92d2a4
Nik Clayton <nik@ngo.org.uk>
2023-01-03 17:48:47 +0100

    Revert 1480c6aa3ac5c0c2d362fb271f47ea2259ab14e2
    Turns out the behaviour is not desired.

--- app/src/main/java/com/keylesspalace/tusky/components/timeline/TimelineFragment.kt
+++ app/src/main/java/com/keylesspalace/tusky/components/timeline/TimelineFragment.kt
@@ -234,9 +234,7 @@ class TimelineFragment :
                 if (positionStart == 0 && adapter.itemCount != itemCount) {
                     binding.recyclerView.post {
                         if (getView() != null) {
-                            // Swiping and prefer oldest-first? Stay at the same position.
-                            // Otherwise jump to the top.
-                            if (isSwipeToRefreshEnabled && readingOrder == ReadingOrder.OLDEST_FIRST) {
+                            if (isSwipeToRefreshEnabled) {
                                 binding.recyclerView.scrollBy(0, Utils.dpToPx(requireContext(), -30))
                             } else binding.recyclerView.scrollToPosition(0)
                         }
