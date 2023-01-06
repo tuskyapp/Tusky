b05751cbddb8f4de7ac3ad4010c4072cda1af18f
Nik Clayton <nik@ngo.org.uk>
2023-01-06 18:42:17 +0100

    Lint

--- app/src/main/java/com/keylesspalace/tusky/components/viewthread/ThreadAdapter.kt
+++ app/src/main/java/com/keylesspalace/tusky/components/viewthread/ThreadAdapter.kt
@@ -15,7 +15,6 @@
 
 package com.keylesspalace.tusky.components.viewthread
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
