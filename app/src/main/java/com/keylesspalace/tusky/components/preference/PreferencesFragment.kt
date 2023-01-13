aa96d02923748e3fd2852f7a172c71d4ff5fb199
Nik Clayton <nik@ngo.org.uk>
2023-01-13 19:28:46 +0100

    Implement HTTP proxy summary as a SummaryProvider (#3091)
    * Handle preference fragments using the framework
    
    The previous code started new preference "screens" as activities, even though
    each one hosted a single fragment.
    
    Modify this to use the framework's support for swapping in/out different
    preference fragments.
    
    PreferencesActivity:
    - Remove the code for launching tab and proxy preferences
    - Remove the code for setting titles, each fragment is responsible for that
    - Implement OnPreferenceStartFragmentCallback to swap fragments in/out with
      the correct animation
    
    PreferencesFragment:
    - Use `fragment` property instead of `setOnPreferenceClickListener`
    - Set the activity title when resuming
    
    Everything else:
    - Set the activity title when resuming
    
    * Implement HTTP proxy summary as a SummaryProvider
    
    Uses the frameworks's support for setting summaries instead of rolling our
    own.
    
    Also fix a tiny bug -- the minimum port number to connect to should be 1,
    not 0.
    
    * Lint

--- app/src/main/java/com/keylesspalace/tusky/components/preference/PreferencesFragment.kt
+++ app/src/main/java/com/keylesspalace/tusky/components/preference/PreferencesFragment.kt
@@ -32,7 +32,6 @@ import com.keylesspalace.tusky.settings.preferenceCategory
 import com.keylesspalace.tusky.settings.switchPreference
 import com.keylesspalace.tusky.util.LocaleManager
 import com.keylesspalace.tusky.util.deserialize
-import com.keylesspalace.tusky.util.getNonNullString
 import com.keylesspalace.tusky.util.makeIcon
 import com.keylesspalace.tusky.util.serialize
 import com.mikepenz.iconics.IconicsDrawable
@@ -49,7 +48,6 @@ class PreferencesFragment : PreferenceFragmentCompat(), Injectable {
     lateinit var localeManager: LocaleManager
 
     private val iconSize by lazy { resources.getDimensionPixelSize(R.dimen.preference_icon_size) }
-    private var httpProxyPref: Preference? = null
 
     enum class ReadingOrder {
         /** User scrolls up, reading statuses oldest to newest */
@@ -236,7 +234,7 @@ class PreferencesFragment : PreferenceFragmentCompat(), Injectable {
             preferenceCategory(R.string.pref_title_timeline_filters) {
                 preference {
                     setTitle(R.string.pref_title_post_tabs)
-                    fragment = "com.keylesspalace.tusky.components.preference.TabFilterPreferencesFragment"
+                    fragment = TabFilterPreferencesFragment::class.qualifiedName
                 }
             }
 
@@ -280,9 +278,10 @@ class PreferencesFragment : PreferenceFragmentCompat(), Injectable {
             }
 
             preferenceCategory(R.string.pref_title_proxy_settings) {
-                httpProxyPref = preference {
+                preference {
                     setTitle(R.string.pref_title_http_proxy_settings)
-                    fragment = "com.keylesspalace.tusky.components.preference.ProxyPreferencesFragment"
+                    fragment = ProxyPreferencesFragment::class.qualifiedName
+                    summaryProvider = ProxyPreferencesFragment.SummaryProvider
                 }
             }
         }
@@ -295,28 +294,6 @@ class PreferencesFragment : PreferenceFragmentCompat(), Injectable {
     override fun onResume() {
         super.onResume()
         requireActivity().setTitle(R.string.action_view_preferences)
-        updateHttpProxySummary()
-    }
-
-    private fun updateHttpProxySummary() {
-        preferenceManager.sharedPreferences?.let { sharedPreferences ->
-            val httpProxyEnabled = sharedPreferences.getBoolean(PrefKeys.HTTP_PROXY_ENABLED, false)
-            val httpServer = sharedPreferences.getNonNullString(PrefKeys.HTTP_PROXY_SERVER, "")
-
-            try {
-                val httpPort = sharedPreferences.getNonNullString(PrefKeys.HTTP_PROXY_PORT, "-1")
-                    .toInt()
-
-                if (httpProxyEnabled && httpServer.isNotBlank() && httpPort > 0 && httpPort < 65535) {
-                    httpProxyPref?.summary = "$httpServer:$httpPort"
-                    return
-                }
-            } catch (e: NumberFormatException) {
-                // user has entered wrong port, fall back to empty summary
-            }
-
-            httpProxyPref?.summary = ""
-        }
     }
 
     override fun onDisplayPreferenceDialog(preference: Preference) {
