127ad3ec644b3b9648c8deab91b1670e08239959
Nik Clayton <nik@ngo.org.uk>
2023-02-13 12:54:13 +0100

    Add Copyright header

--- app/src/main/java/com/keylesspalace/tusky/di/ViewModelFactory.kt
+++ app/src/main/java/com/keylesspalace/tusky/di/ViewModelFactory.kt
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2023 Tusky Contributors
+ *
+ * This file is a part of Tusky.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Tusky is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Tusky; if not,
+ * see <http://www.gnu.org/licenses>.
+ */
+
 // from https://proandroiddev.com/viewmodel-with-dagger2-architecture-components-2e06f06c9455
 
 package com.keylesspalace.tusky.di
