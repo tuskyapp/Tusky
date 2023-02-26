f81012da8a1cd59d62e3a9f6a4102904dc13d0b6
Nik Clayton <nik@ngo.org.uk>
2023-02-26 11:16:02 +0100

    Move compose.* tests to own namespace

--- app/src/test/java/com/keylesspalace/tusky/components/compose/ComposeTokenizer/ComposeTokenizerTest.kt
+++ app/src/test/java/com/keylesspalace/tusky/components/compose/ComposeTokenizer/ComposeTokenizerTest.kt
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License along with Tusky; if not,
  * see <http://www.gnu.org/licenses>. */
 
-package com.keylesspalace.tusky
+package com.keylesspalace.tusky.components.compose.ComposeTokenizer
 
 import com.keylesspalace.tusky.components.compose.ComposeTokenizer
 import org.junit.Assert
