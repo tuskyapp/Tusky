dacbbd1b03eb518bd674f0cc0ddad2ff9fa685e5
Goooler <wangzongler@gmail.com>
2023-02-26 14:48:57 +0800

    settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,10 +10,10 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url 'https://jitpack.io' }
+        maven("https://jitpack.io")
     }
 }
 
 enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
 
-include ':app'
+include(":app")
