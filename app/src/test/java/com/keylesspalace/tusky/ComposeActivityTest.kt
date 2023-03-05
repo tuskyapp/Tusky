c9f289de456a91b813a3b8c69b3dcaff3554678d
Nik Clayton <nik@ngo.org.uk>
2023-03-05 22:39:46 +0100

    model/InstanceDao

--- app/src/test/java/com/keylesspalace/tusky/ComposeActivityTest.kt
+++ app/src/test/java/com/keylesspalace/tusky/ComposeActivityTest.kt
@@ -23,6 +23,7 @@ import at.connyduck.calladapter.networkresult.NetworkResult
 import com.keylesspalace.tusky.components.compose.ComposeActivity
 import com.keylesspalace.tusky.components.compose.ComposeViewModel
 import com.keylesspalace.tusky.components.instanceinfo.InstanceInfoRepository
+import com.keylesspalace.tusky.core.database.dao.InstanceDao
 import com.keylesspalace.tusky.core.database.model.AccountEntity
 import com.keylesspalace.tusky.core.database.model.EmojisEntity
 import com.keylesspalace.tusky.core.database.model.Instance
@@ -31,7 +32,6 @@ import com.keylesspalace.tusky.core.database.model.InstanceInfoEntity
 import com.keylesspalace.tusky.core.database.model.StatusConfiguration
 import com.keylesspalace.tusky.db.AccountManager
 import com.keylesspalace.tusky.db.AppDatabase
-import com.keylesspalace.tusky.db.InstanceDao
 import com.keylesspalace.tusky.di.ViewModelFactory
 import com.keylesspalace.tusky.network.MastodonApi
 import org.junit.Assert.assertEquals
--- core/database/src/main/java/com/keylesspalace/tusky/core/database/dao/InstanceDao.kt
+++ core/database/src/main/java/com/keylesspalace/tusky/core/database/dao/InstanceDao.kt
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License along with Tusky; if not,
  * see <http://www.gnu.org/licenses>. */
 
-package com.keylesspalace.tusky.db
+package com.keylesspalace.tusky.core.database.dao
 
 import androidx.room.Dao
 import androidx.room.Query
