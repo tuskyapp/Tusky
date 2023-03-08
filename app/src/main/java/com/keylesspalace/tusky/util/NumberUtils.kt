1c74933d131e8439bca0c7166735a79acb93899a
grigoriiioffe <zikasaks@icloud.com>
2023-03-08 21:41:54 +0200

    Use kotlin functions

--- app/src/main/java/com/keylesspalace/tusky/util/NumberUtils.kt
+++ app/src/main/java/com/keylesspalace/tusky/util/NumberUtils.kt
@@ -1,13 +1,16 @@
 package com.keylesspalace.tusky.util
 
 import java.text.DecimalFormat
+import kotlin.math.floor
+import kotlin.math.log10
+import kotlin.math.pow
 
 fun shortNumber(number: Number): String {
     val array = arrayOf(' ', 'k', 'M', 'B', 'T', 'P', 'E')
-    val value = Math.floor(Math.log10(number.toDouble())).toInt()
+    val value = floor(log10(number.toDouble())).toInt()
     val base = value / 3
     if (value >= 3 && base < array.size) {
-        return DecimalFormat("#0.0").format(number.toDouble() / Math.pow(10.0, (base * 3).toDouble())) + array[base]
+        return DecimalFormat("#0.0").format(number.toDouble() / 10.0.pow((base * 3).toDouble())) + array[base]
     } else {
         return DecimalFormat("#,##0").format(number)
     }
