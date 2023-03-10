1442aa83c9ba41c4ffd462768a931d3e9fb25ad9
grigoriiioffe <zikasaks@icloud.com>
2023-03-10 14:38:21 +0200

    Use capital letters for all variants

--- app/src/main/java/com/keylesspalace/tusky/util/NumberUtils.kt
+++ app/src/main/java/com/keylesspalace/tusky/util/NumberUtils.kt
@@ -6,7 +6,7 @@ import kotlin.math.log10
 import kotlin.math.pow
 
 fun shortNumber(number: Number): String {
-    val array = arrayOf(' ', 'k', 'M', 'B', 'T', 'P', 'E')
+    val array = arrayOf(' ', 'K', 'M', 'B', 'T', 'P', 'E')
     val value = floor(log10(number.toDouble())).toInt()
     val base = value / 3
     if (value >= 3 && base < array.size) {
