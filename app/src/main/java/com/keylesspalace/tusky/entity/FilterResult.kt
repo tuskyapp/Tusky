c91237c82fc3b1a3b63acd28edf74cc6a90ea040
Nik Clayton <nik@ngo.org.uk>
2023-05-08 21:37:25 +0200

    FilterResult.status_matches is a List<String>?
    See https://github.com/mastodon/documentation/pull/1214

--- app/src/main/java/com/keylesspalace/tusky/entity/FilterResult.kt
+++ app/src/main/java/com/keylesspalace/tusky/entity/FilterResult.kt
@@ -5,14 +5,5 @@ import com.google.gson.annotations.SerializedName
 data class FilterResult(
     val filter: Filter,
     @SerializedName("keyword_matches") val keywordMatches: List<String>?,
-
-    // `statusMatches` is commented out because it is unclear what the correct type should be.
-    // The Mastodon documentation is inconsistent, it's either a String?, or a List<String>?.
-    // Getting this wrong crashes during JSON parsing.
-    //
-    // Since the field isn't used anywhere in Tusky yet it's easier to comment this out so
-    // it is not deserialized.
-    //
-    // This can be restored when https://github.com/mastodon/mastodon/issues/24900 is fixed.
-    //    @SerializedName("status_matches") val statusMatches: String?
+    @SerializedName("status_matches") val statusMatches: List<String>?
 )
