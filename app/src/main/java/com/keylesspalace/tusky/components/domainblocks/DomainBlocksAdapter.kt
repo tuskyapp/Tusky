2393ff1e4d3ff99235ba89c4c697c0ace04e483f
Conny Duck <git@connyduck.at>
2023-07-05 19:23:04 +0200

    rename item_muted_domain to item_blocked_domain

--- app/src/main/java/com/keylesspalace/tusky/components/domainblocks/DomainBlocksAdapter.kt
+++ app/src/main/java/com/keylesspalace/tusky/components/domainblocks/DomainBlocksAdapter.kt
@@ -4,22 +4,22 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
 import com.keylesspalace.tusky.components.followedtags.FollowedTagsAdapter.Companion.STRING_COMPARATOR
-import com.keylesspalace.tusky.databinding.ItemMutedDomainBinding
+import com.keylesspalace.tusky.databinding.ItemBlockedDomainBinding
 import com.keylesspalace.tusky.util.BindingHolder
 
 class DomainBlocksAdapter(
     private val onUnmute: (String) -> Unit
-) : PagingDataAdapter<String, BindingHolder<ItemMutedDomainBinding>>(STRING_COMPARATOR) {
+) : PagingDataAdapter<String, BindingHolder<ItemBlockedDomainBinding>>(STRING_COMPARATOR) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingHolder<ItemMutedDomainBinding> {
-        val binding = ItemMutedDomainBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingHolder<ItemBlockedDomainBinding> {
+        val binding = ItemBlockedDomainBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return BindingHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: BindingHolder<ItemMutedDomainBinding>, position: Int) {
+    override fun onBindViewHolder(holder: BindingHolder<ItemBlockedDomainBinding>, position: Int) {
         getItem(position)?.let { instance ->
-            holder.binding.mutedDomain.text = instance
-            holder.binding.mutedDomainUnmute.setOnClickListener {
+            holder.binding.blockedDomain.text = instance
+            holder.binding.blockedDomainUnblock.setOnClickListener {
                 onUnmute(instance)
             }
         }
