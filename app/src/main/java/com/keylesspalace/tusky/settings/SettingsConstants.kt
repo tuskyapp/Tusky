e09e857cadaaa59386775513d4a2bc8d4e6a38fe
sanao1006 <naosak1006@gmail.com>
2023-11-06 15:22:41 +0900

    feat: Hide self-boosts (#3534)
    Some Mastodon posters have a annoying habit of boosting their own posts some time after they've posted them.
     No need to see the same toot over and over again.
    
     Therefore, add an option to show these self-boosts in the preferences (default: on).
    
     Fixes #3534

--- app/src/main/java/com/keylesspalace/tusky/settings/SettingsConstants.kt
+++ app/src/main/java/com/keylesspalace/tusky/settings/SettingsConstants.kt
@@ -106,6 +106,7 @@ object PrefKeys {
 
     const val TAB_FILTER_HOME_REPLIES = "tabFilterHomeReplies_v2" // This was changed once to reset an unintentionally set default.
     const val TAB_FILTER_HOME_BOOSTS = "tabFilterHomeBoosts"
+    const val TAB_SHOW_HOME_SELF_BOOSTS = "tabShowHomeSelfBoosts"
 
     /** UI text scaling factor, stored as float, 100 = 100% = no scaling */
     const val UI_TEXT_SCALE_RATIO = "uiTextScaleRatio"
