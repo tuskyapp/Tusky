5d050b522c1c15e1f41085dcded36bcf547c8696
Conny Duck <git@connyduck.at>
2023-11-25 12:55:22 +0100

    fix ktlint

--- app/src/main/java/com/keylesspalace/tusky/components/compose/dialog/CaptionDialog.kt
+++ app/src/main/java/com/keylesspalace/tusky/components/compose/dialog/CaptionDialog.kt
@@ -24,7 +24,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
-import android.widget.EditText
 import android.widget.LinearLayout
 import androidx.core.os.BundleCompat
 import androidx.core.os.bundleOf
