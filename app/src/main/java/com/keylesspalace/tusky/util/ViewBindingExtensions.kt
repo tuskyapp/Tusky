17990526c1804a422aa0e950ebfb4b61b81d2c0c
Christophe Beyls <software@digitalia.be>
2024-02-23 17:20:06 +0100

    add URL to the article explaining the refactoring rationale

--- app/src/main/java/com/keylesspalace/tusky/util/ViewBindingExtensions.kt
+++ app/src/main/java/com/keylesspalace/tusky/util/ViewBindingExtensions.kt
@@ -10,7 +10,8 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.viewbinding.ViewBinding
 
 /**
- * https://medium.com/@Zhuinden/simple-one-liner-viewbinding-in-fragments-and-activities-with-kotlin-961430c6c07c
+ * Original code: https://medium.com/@Zhuinden/simple-one-liner-viewbinding-in-fragments-and-activities-with-kotlin-961430c6c07c
+ * Refactor: https://bladecoder.medium.com/viewlifecyclelazy-and-other-ways-to-avoid-view-memory-leaks-in-android-fragments-4aa982e6e579
  */
 
 inline fun <T : ViewBinding> AppCompatActivity.viewBinding(
