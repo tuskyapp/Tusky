29e7d3dac41815d45432854e61c3f6e6950fcfd8
Christophe Beyls <software@digitalia.be>
2024-02-27 17:40:38 +0100

    fix Glide submitAsync() extension

--- app/src/main/java/com/keylesspalace/tusky/util/GlideExtensions.kt
+++ app/src/main/java/com/keylesspalace/tusky/util/GlideExtensions.kt
@@ -1,13 +1,10 @@
 package com.keylesspalace.tusky.util
 
-import android.graphics.drawable.Drawable
 import com.bumptech.glide.RequestBuilder
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.target.Target
-import com.bumptech.glide.request.transition.Transition
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlinx.coroutines.suspendCancellableCoroutine
@@ -21,37 +18,30 @@ suspend fun <R> RequestBuilder<R>.submitAsync(
     height: Int = Int.MIN_VALUE
 ): R {
     return suspendCancellableCoroutine { continuation ->
-        val target = object : CustomTarget<R>(width, height), RequestListener<R> {
-            override fun onResourceReady(resource: R & Any, transition: Transition<in R>?) {
-                // Do nothing, we use the RequestListener version instead
-            }
+        val target = addListener(
+            object : RequestListener<R> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<R>,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    continuation.resumeWithException(e ?: IOException("Image loading failed"))
+                    return false
+                }
 
-            override fun onLoadCleared(placeholder: Drawable?) {
-                // Do nothing, we don't retain a reference to the resource
+                override fun onResourceReady(
+                    resource: R & Any,
+                    model: Any,
+                    target: Target<R>?,
+                    dataSource: DataSource,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    continuation.resume(resource)
+                    return false
+                }
             }
-
-            override fun onLoadFailed(
-                e: GlideException?,
-                model: Any?,
-                target: Target<R>,
-                isFirstResource: Boolean
-            ): Boolean {
-                continuation.resumeWithException(e ?: IOException("Image loading failed"))
-                return false
-            }
-
-            override fun onResourceReady(
-                resource: R & Any,
-                model: Any,
-                target: Target<R>?,
-                dataSource: DataSource,
-                isFirstResource: Boolean
-            ): Boolean {
-                continuation.resume(resource)
-                return false
-            }
-        }
-        into(target)
-        continuation.invokeOnCancellation { target.request?.clear() }
+        ).submit(width, height)
+        continuation.invokeOnCancellation { target.cancel(true) }
     }
 }
