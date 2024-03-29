b022767ae6740efa367cf64c953e767d98fac212
Konrad Pozniak <connyduck@users.noreply.github.com>
2024-03-29 21:13:57 +0100

    fix translating polls and spoilers (#4344)
    The docs are wrong https://github.com/mastodon/documentation/pull/1423 🙄

--- app/src/main/java/com/keylesspalace/tusky/entity/Translation.kt
+++ app/src/main/java/com/keylesspalace/tusky/entity/Translation.kt
@@ -14,12 +14,20 @@ data class MediaTranslation(
  */
 data class Translation(
     val content: String,
-    @SerializedName("spoiler_warning")
-    val spoilerWarning: String?,
-    val poll: List<String>?,
+    @SerializedName("spoiler_text")
+    val spoilerText: String?,
+    val poll: TranslatedPoll?,
     @SerializedName("media_attachments")
     val mediaAttachments: List<MediaTranslation>,
     @SerializedName("detected_source_language")
     val detectedSourceLanguage: String,
     val provider: String,
+)
+
+data class TranslatedPoll(
+    val options: List<TranslatedPollOption>
+)
+
+data class TranslatedPollOption(
+    val title: String
 )
