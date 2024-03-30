9fc32b2d9f94877d1f8eef980d72c8c0d5fd1672
Christophe Beyls <software@digitalia.be>
2024-03-30 18:38:12 +0100

    fix nullable fields declared as non-null

--- app/src/main/java/com/keylesspalace/tusky/entity/FilterResult.kt
+++ app/src/main/java/com/keylesspalace/tusky/entity/FilterResult.kt
@@ -6,6 +6,6 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class FilterResult(
     val filter: Filter,
-    @Json(name = "keyword_matches") val keywordMatches: List<String> = emptyList(),
-    @Json(name = "status_matches") val statusMatches: List<String> = emptyList()
+    @Json(name = "keyword_matches") val keywordMatches: List<String>? = null,
+    @Json(name = "status_matches") val statusMatches: List<String>? = null
 )
