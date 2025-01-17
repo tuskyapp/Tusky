3db8e0b5a33f33afa448b8323c308d953b45795f
Konrad Pozniak <connyduck@users.noreply.github.com>
2025-01-17 12:41:45 +0100

    center icon in small send button (#4875)
    2 users complained already 😔 
    
    Before:
    
    ![before](https://github.com/user-attachments/assets/d3472856-a01e-42c6-b477-88fc376c63de)
    
    After:
    
    ![after](https://github.com/user-attachments/assets/6722dac7-59d3-47b7-9718-6caae5eb0bec)

--- app/src/main/java/com/keylesspalace/tusky/components/compose/view/TootButton.kt
+++ app/src/main/java/com/keylesspalace/tusky/components/compose/view/TootButton.kt
@@ -38,9 +38,9 @@ class TootButton
     init {
         if (smallStyle) {
             setIconResource(R.drawable.ic_send_24dp)
+            iconPadding = 0
         } else {
             setText(R.string.action_send)
-            iconGravity = ICON_GRAVITY_TEXT_START
         }
         val padding = resources.getDimensionPixelSize(R.dimen.toot_button_horizontal_padding)
         setPadding(padding, 0, padding, 0)
