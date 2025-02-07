tuskyapp/Tusky

commit b24427c6dfa4821608fc2b0d6ea99e179cf287a8

fix diffing statuses (#4909)
Several cases of payloads not being forwarded correctly, leading to
unnecessary re-binding of the whole status view. I simplified the logic
by removing the additional list level, making it easier to understand
what is going on.

app/src/main/java/com/keylesspalace/tusky/components/conversation/ConversationViewHolder.java
@@ -69,7 +69,7 @@ public class ConversationViewHolder extends StatusBaseViewHolder {
 
     void setupWithConversation(
             @NonNull ConversationViewData conversation,
-            @Nullable Object payloads
+            @Nullable List<Object> payloads
     ) {
 
         StatusViewData.Concrete statusViewData = conversation.getLastStatus();
@@ -118,11 +118,9 @@ public class ConversationViewHolder extends StatusBaseViewHolder {
 
             setAvatars(conversation.getAccounts());
         } else {
-            if (payloads instanceof List) {
-                for (Object item : (List<?>) payloads) {
-                    if (Key.KEY_CREATED.equals(item)) {
-                        setMetaData(statusViewData, statusDisplayOptions, listener);
-                    }
+            for (Object item : payloads) {
+                if (Key.KEY_CREATED.equals(item)) {
+                    setMetaData(statusViewData, statusDisplayOptions, listener);
                 }
             }
         }

app/src/main/java/com/keylesspalace/tusky/adapter/FilteredStatusViewHolder.kt
@@ -40,7 +40,11 @@ class FilteredStatusViewHolder(
         viewData: NotificationViewData.Concrete,
         payloads: List<*>,
         statusDisplayOptions: StatusDisplayOptions
-    ) = bind(viewData.statusViewData!!)
+    ) {
+        if (payloads.isEmpty()) {
+            bind(viewData.statusViewData!!)
+        }
+    }
 
     fun bind(viewData: StatusViewData.Concrete) {
         val matchedFilterResult: FilterResult? = viewData.actionable.filtered.orEmpty().find { filterResult ->

app/src/main/java/com/keylesspalace/tusky/adapter/FollowRequestViewHolder.kt
@@ -49,6 +49,9 @@ class FollowRequestViewHolder(
         payloads: List<*>,
         statusDisplayOptions: StatusDisplayOptions
     ) {
+        if (payloads.isNotEmpty()) {
+            return
+        }
         setupWithAccount(
             viewData.account,
             statusDisplayOptions.animateAvatars,

app/src/main/java/com/keylesspalace/tusky/adapter/StatusBaseViewHolder.java
@@ -779,9 +779,9 @@ public abstract class StatusBaseViewHolder extends RecyclerView.ViewHolder {
     public void setupWithStatus(@NonNull StatusViewData.Concrete status,
                                 @NonNull final StatusActionListener listener,
                                 @NonNull StatusDisplayOptions statusDisplayOptions,
-                                @Nullable Object payloads,
+                                @NonNull List<Object> payloads,
                                 final boolean showStatusInfo) {
-        if (payloads == null) {
+        if (payloads.isEmpty()) {
             Status actionable = status.getActionable();
             setDisplayName(actionable.getAccount().getName(), actionable.getAccount().getEmojis(), statusDisplayOptions);
             setUsername(actionable.getAccount().getUsername());
@@ -832,16 +832,16 @@ public abstract class StatusBaseViewHolder extends RecyclerView.ViewHolder {
             // and let RecyclerView ask for a new delegate.
             itemView.setAccessibilityDelegate(null);
         } else {
-            if (payloads instanceof List)
-                for (Object item : (List<?>) payloads) {
-                    if (Key.KEY_CREATED.equals(item)) {
-                        setMetaData(status, statusDisplayOptions, listener);
-                        if (status.getStatus().getCard() != null && status.getStatus().getCard().getPublishedAt() != null) {
-                            // there is a preview card showing the published time, we need to refresh it as well
-                            setupCard(status, status.isExpanded(), statusDisplayOptions.cardViewMode(), statusDisplayOptions, listener);
-                        }
+            for (Object item : payloads) {
+                if (Key.KEY_CREATED.equals(item)) {
+                    setMetaData(status, statusDisplayOptions, listener);
+                    if (status.getStatus().getCard() != null && status.getStatus().getCard().getPublishedAt() != null) {
+                        // there is a preview card showing the published time, we need to refresh it as well
+                        setupCard(status, status.isExpanded(), statusDisplayOptions.cardViewMode(), statusDisplayOptions, listener);
                     }
+                    break;
                 }
+            }
         }
     }
 
@@ -1239,7 +1239,7 @@ public abstract class StatusBaseViewHolder extends RecyclerView.ViewHolder {
                     builder = builder.placeholder(decodeBlurHash(card.getBlurhash()));
                 }
                 builder.centerInside()
-                  .into(cardImage);
+                    .into(cardImage);
             } else if (statusDisplayOptions.useBlurhash() && !TextUtils.isEmpty(card.getBlurhash())) {
                 int radius = cardImage.getContext().getResources()
                     .getDimensionPixelSize(R.dimen.inner_card_radius);

app/src/main/java/com/keylesspalace/tusky/adapter/StatusDetailedViewHolder.java
@@ -27,6 +27,7 @@ import com.keylesspalace.tusky.viewdata.StatusViewData;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 
 public class StatusDetailedViewHolder extends StatusBaseViewHolder {
     private final TextView reblogs;
@@ -140,7 +141,7 @@ public class StatusDetailedViewHolder extends StatusBaseViewHolder {
     public void setupWithStatus(@NonNull final StatusViewData.Concrete status,
                                 @NonNull final StatusActionListener listener,
                                 @NonNull StatusDisplayOptions statusDisplayOptions,
-                                @Nullable Object payloads,
+                                @NonNull List<Object> payloads,
                                 final boolean showStatusInfo) {
         // We never collapse statuses in the detail view
         StatusViewData.Concrete uncollapsedStatus = (status.isCollapsible() && status.isCollapsed()) ?
@@ -149,7 +150,7 @@ public class StatusDetailedViewHolder extends StatusBaseViewHolder {
 
         super.setupWithStatus(uncollapsedStatus, listener, statusDisplayOptions, payloads, showStatusInfo);
         setupCard(uncollapsedStatus, status.isExpanded(), CardViewMode.FULL_WIDTH, statusDisplayOptions, listener); // Always show card for detailed status
-        if (payloads == null) {
+        if (payloads.isEmpty()) {
             Status actionable = uncollapsedStatus.getActionable();
 
             if (!statusDisplayOptions.hideStats()) {

app/src/main/java/com/keylesspalace/tusky/adapter/StatusViewHolder.java
@@ -39,6 +39,7 @@ import com.keylesspalace.tusky.util.StringUtils;
 import com.keylesspalace.tusky.viewdata.StatusViewData;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 import at.connyduck.sparkbutton.helpers.Utils;
@@ -64,9 +65,9 @@ public class StatusViewHolder extends StatusBaseViewHolder {
     public void setupWithStatus(@NonNull StatusViewData.Concrete status,
                                 @NonNull final StatusActionListener listener,
                                 @NonNull StatusDisplayOptions statusDisplayOptions,
-                                @Nullable Object payloads,
+                                @NonNull List<Object> payloads,
                                 final boolean showStatusInfo) {
-        if (payloads == null) {
+        if (payloads.isEmpty()) {
             boolean sensitive = !TextUtils.isEmpty(status.getActionable().getSpoilerText());
             boolean expanded = status.isExpanded();
 

app/src/main/java/com/keylesspalace/tusky/components/conversation/ConversationAdapter.kt
@@ -48,13 +48,9 @@ class ConversationAdapter(
         onBindViewHolder(holder, position, emptyList())
     }
 
-    override fun onBindViewHolder(
-        holder: ConversationViewHolder,
-        position: Int,
-        payloads: List<Any>
-    ) {
+    override fun onBindViewHolder(holder: ConversationViewHolder, position: Int, payloads: List<Any>) {
         getItem(position)?.let { conversationViewData ->
-            holder.setupWithConversation(conversationViewData, payloads.firstOrNull())
+            holder.setupWithConversation(conversationViewData, payloads)
         }
     }
 
@@ -80,7 +76,7 @@ class ConversationAdapter(
             ): Any? {
                 return if (oldItem == newItem) {
                     // If items are equal - update timestamp only
-                    listOf(StatusBaseViewHolder.Key.KEY_CREATED)
+                    StatusBaseViewHolder.Key.KEY_CREATED
                 } else {
                     // If items are different - update the whole view holder
                     null

app/src/main/java/com/keylesspalace/tusky/components/conversation/ConversationsFragment.kt
@@ -184,7 +184,7 @@ class ConversationsFragment :
                     adapter.notifyItemRangeChanged(
                         0,
                         adapter.itemCount,
-                        listOf(StatusBaseViewHolder.Key.KEY_CREATED)
+                        StatusBaseViewHolder.Key.KEY_CREATED
                     )
                     delay(1.toDuration(DurationUnit.MINUTES))
                 }

commit b42c1e8eb50e9e02256ae622ab4fa91b450c725c

fix conversation tab not having any content (#4911)
regression from https://github.com/tuskyapp/Tusky/pull/4909

app/src/main/java/com/keylesspalace/tusky/components/conversation/ConversationViewHolder.java
@@ -69,13 +69,13 @@ public class ConversationViewHolder extends StatusBaseViewHolder {
 
     void setupWithConversation(
             @NonNull ConversationViewData conversation,
-            @Nullable List<Object> payloads
+            @NonNull List<Object> payloads
     ) {
 
         StatusViewData.Concrete statusViewData = conversation.getLastStatus();
         Status status = statusViewData.getStatus();
 
-        if (payloads == null) {
+        if (payloads.isEmpty()) {
             TimelineAccount account = status.getAccount();
 
             setupCollapsedState(statusViewData.isCollapsible(), statusViewData.isCollapsed(), statusViewData.isExpanded(), status.getSpoilerText(), listener);